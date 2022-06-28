96d141df4e1e4a605e3c5f338d3ede66e18c9f41
Myeongseon Shim <rocksea@amazon.com>
2022-06-29 03:13:21 +0900

    change helm resources

--- coupon-api/src/main/java/kr/co/sample/coupon/presentation/event/IssueCoupon.java
+++ coupon-api/src/main/java/kr/co/sample/coupon/presentation/event/IssueCoupon.java
@@ -2,14 +2,14 @@
  * Copyright 2021 ROCKSEA. All rights Reserved.
  * ROCKSEA PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package kr.co.sample.coupon.application;
+package kr.co.sample.coupon.presentation.event;
 
 import java.io.IOException;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class IssueCoupon {
     @KafkaListener(topics = "issue_coupon", groupId = "coupon")
     public void consume(String message) throws IOException {
--- helmchart/install-member.sh
+++ helmchart/install-member.sh
@@ -1,22 +1,38 @@
 export IMAGE_REPO_URL='320811633077.dkr.ecr.ap-northeast-2.amazonaws.com'
 export IMAGE_REPO_NAME="sample-member-api"
-export IMAGE_TAG='8dca5da'
+export IMAGE_TAG='blue'
+export VERSION='v1.0.0'
 export BRANCH_NAME='main'
 export DOMAIN_NAME="rocksea.achromaticscenery.com"
 export HOST_NAME="member.dev.${DOMAIN_NAME}"
 export SERVICE_NAME='member-api'
 export ENV='dev'
 export HELM_EXPERIMENTAL_OCI=1 
+export ECR_PASS=`aws ecr get-login-password --region ap-northeast-2`
+export ARGOCD_PASS=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo`
+export ARGOCD_URL='ac140c54269d04d44a8edaf79a0a9676-312392554.ap-northeast-2.elb.amazonaws.com'
 
 # Packaging Chart
-cat member-api/values.template.yaml | envsubst > member-api/values.yaml
-helm package member-api --version 0.0.1-${BRANCH_NAME}
+cat ${SERVICE_NAME}/values.template.yaml | envsubst > ${SERVICE_NAME}/values.yaml
+helm package ${SERVICE_NAME} --version ${VERSION}
 
 # Login to ECR
-aws ecr get-login-password --region ap-northeast-2 | helm registry login --username AWS --password-stdin $IMAGE_REPO_URL
+aws ecr get-login-password --region ap-northeast-2 | helm registry login --username AWS --password-stdin ${IMAGE_REPO_URL}
 
 # Push to ECR
-helm push member-api-0.0.1-${BRANCH_NAME}.tgz oci://$IMAGE_REPO_URL
+helm push ${SERVICE_NAME}-${VERSION}.tgz oci://${IMAGE_REPO_URL}
 
-#helm uninstall member-api -ndev
-envsubst < member-api/values.yaml | helm install member-api ./member-api -n dev --values -
+#helm uninstall ${SERVICE_NAME} -ndev
+#envsubst < ${SERVICE_NAME}/values.yaml | helm install ${SERVICE_NAME} ./${SERVICE_NAME} -n dev --values -
+
+# Login to Argocd
+argocd login --insecure ${ARGOCD_URL} --username 'admin' --password ${ARGOCD_PASS} 
+
+# add a repository
+argocd repo add ${IMAGE_REPO_URL} --enable-oci --username AWS --type helm --password ${ECR_PASS} --name eks --upsert
+
+# create an application
+argocd app create ${SERVICE_NAME} --repo ${IMAGE_REPO_URL} --helm-chart ${SERVICE_NAME} --revision ${VERSION} --dest-namespace dev --dest-server https://kubernetes.default.svc --sync-policy auto --upsert
+
+# remove manifest cache
+argocd app get ${SERVICE_NAME} --hard-refresh
